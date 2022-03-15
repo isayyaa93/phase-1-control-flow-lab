0c67d9cfc0f7724f3fe434d26bac63bc096bc104
Isaiah Kang <i.say.yaa@gmail.com>
2022-03-15 14:18:25 -0400

    commit tree 9299544b790fc5ccc445943eed52d4eb0cc01fa7

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+let distance = 199
+
+function scuberGreetingForFeet(distance){
+  
   // Write your code here!
+  if(distance <= 400){
+    return 'This one is on me!';
+  }
+
+  
+  else if(distance >= 2000 && distance < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  }
+  else if (city !== "NYC"){
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tip;
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "not as generous":
+      return "Thank you."
+    break;
+    case "generous":
+      return "Thank you so much."
+    break;
+    default:
+      return "Bye."
+  }
 }
